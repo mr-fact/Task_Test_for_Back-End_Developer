63c62fcb9533c211008e2b369d761dd60eb73cb7
mr-fact <mr.fact.202020@gmail.com>
2024-07-02 11:47:23 +0330

    Add Project viewset with CRUD methods
    - Implemented the ProjectViewSet class
    - Included all CRUD methods: create, retrieve, update, delete
    - Integrated with the ProjectSerializer
    - Add paths for Swagger schema and UI

--- taski/taski/project/urls.py
+++ taski/taski/project/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+from taski.project.views import ProjectViewSet
+
+router = DefaultRouter()
+router.register(r'', ProjectViewSet, basename='projects')
 
 urlpatterns = [
 
-]
+] + router.urls
--- taski/taski/project/views.py
+++ taski/taski/project/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema_view, extend_schema
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from taski.project.models import Project
+from taski.project.serializers import ProjectSerializer
+
+
+@extend_schema_view(
+    list=extend_schema(tags=['Project']),
+    retrieve=extend_schema(tags=['Project']),
+    create=extend_schema(tags=['Project']),
+    update=extend_schema(tags=['Project']),
+    partial_update=extend_schema(tags=['Project']),
+    destroy=extend_schema(tags=['Project']),
+)
+class ProjectViewSet(ModelViewSet):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
