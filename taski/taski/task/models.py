1f244dd061f397610b9a6f84a8e02b532e77025a
mr-fact <mr.fact.202020@gmail.com>
2024-07-02 11:13:00 +0330

    create Comment model

--- taski/taski/task/models.py
+++ taski/taski/task/models.py
@@ -20,3 +20,12 @@ class Task(BaseModel):
 
     def __str__(self):
         return f'{self.id}-{self.title}'
+
+
+class Comment(BaseModel):
+    task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=255)
+    content = models.TextField()
+
+    def __str__(self):
+        return f'{self.id}-{self.task.title}'
