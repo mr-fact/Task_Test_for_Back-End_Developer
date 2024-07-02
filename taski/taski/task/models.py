69556ba316cf8404faeeec69ce2dd57f8cf72e97
mr-fact <mr.fact.202020@gmail.com>
2024-07-02 11:11:49 +0330

    create Task model

--- taski/taski/task/models.py
+++ taski/taski/task/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from taski.common.models import BaseModel
+from taski.project.models import Project
 
-# Create your models here.
+
+class Task(BaseModel):
+    project = models.ForeignKey(Project, related_name='tasks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(
+        max_length=15,
+        choices=[
+            ('pending', 'Pending'),
+            ('in_progress', 'InProgress'),
+            ('completed', 'Completed')
+        ],
+        default='pending'
+    )
+    due_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.id}-{self.title}'
