fcd9034df5b9c7a26ac2bd7254cce7e7dcad3615
mr-fact <mr.fact.202020@gmail.com>
2024-07-02 13:13:22 +0330

    remove extra imports

--- taski/taski/project/urls.py
+++ taski/taski/project/urls.py
@@ -1,6 +1,4 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from taski.project.views import ProjectViewSet
 
