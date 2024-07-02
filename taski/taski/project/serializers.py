9ee083444a3ffa621c9a99729fee58371e99f844
mr-fact <mr.fact.202020@gmail.com>
2024-07-02 13:06:38 +0330

    add slug validation to project serializer

--- taski/taski/project/serializers.py
+++ taski/taski/project/serializers.py
@@ -1,12 +1,22 @@
+from django.utils.text import slugify
 from rest_framework import serializers
 
 from taski.project.models import Project
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(read_only=True)
+
     class Meta:
         model = Project
         fields = (
+            'id',
+            'slug',
             'name',
             'description',
         )
+
+    def validate(self, attrs):
+        if not self.instance and Project.objects.filter(slug=slugify(attrs.get('name'))).exists():
+            raise serializers.ValidationError({'slug': 'this slug already exists'})
+        return attrs
