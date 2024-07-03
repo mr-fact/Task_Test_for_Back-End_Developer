5a3988cb0616c79fd4936a597846ce4439a36edd
mr-fact <mr.fact.202020@gmail.com>
2024-07-03 13:19:40 +0330

    create input and putput comment serializers

--- taski/taski/task/serialziers.py
+++ taski/taski/task/serialziers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
 from taski.project.models import Project
-from taski.task.models import Task
+from taski.task.models import Task, Comment
+from taski.task.services import add_new_comment_to_task
 
 
 class ProjectFieldSerializer(serializers.ModelSerializer):
@@ -35,4 +36,49 @@ class TaskInputSerializer(serializers.ModelSerializer):
             'status',
             'due_date',
             'project'
+        )
+
+
+class TaskFieldSerializer(serializers.ModelSerializer):
+    project = ProjectFieldSerializer(read_only=True)
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+
+
+class CommentOutPutSerializer(serializers.ModelSerializer):
+    task = TaskFieldSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        depth = 1
+        fields = (
+            'author',
+            'content',
+            'task',
+        )
+
+
+class CommentInputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = (
+            'author',
+            'content',
+        )
+
+    def validate(self, attrs):
+        try:
+            task = self.context.get('task')
+            attrs['task'] = Task.objects.get(id=task)
+        except Task.DoesNotExist:
+            raise serializers.ValidationError({'task': 'task not found'})
+        return attrs
+
+    def create(self, validated_data):
+        return add_new_comment_to_task(
+            validated_data.get('author'),
+            validated_data.get('content'),
+            validated_data.get('task'),
         )
