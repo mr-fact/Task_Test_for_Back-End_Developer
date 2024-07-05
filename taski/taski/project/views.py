1324a49d93070f0c765a2f62e40a36183fed2ac1
mr-fact <mr.fact.202020@gmail.com>
2024-07-05 15:21:54 +0330

    add redis to project viewset

--- taski/taski/project/views.py
+++ taski/taski/project/views.py
@@ -1,4 +1,6 @@
+from django.core.cache import cache
 from drf_spectacular.utils import extend_schema_view, extend_schema
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from taski.project.models import Project
@@ -16,3 +18,33 @@ from taski.project.serializers import ProjectSerializer
 class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        data = self.get_all_from_cache(request)
+        if not data:
+            data = self.get_all_from_db(request)
+        return Response(data)
+
+    def get_all_from_cache(self, request):
+        data = cache.get(f'all_projects_{request.GET}')
+        return data
+
+    def get_all_from_db(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        data = serializer.data
+        cache.set(f'all_projects_{str(request.GET)}', data, 600)  # 10 min / 600 sec
+        return data
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        if response.status_code == 201:
+            keys = cache.keys('all_projects_*')
+            for key in keys:
+                cache.delete(key)
+        return response
